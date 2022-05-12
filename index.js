3234adf7e15a7b2ff2b5480b8ec213a842f18210
Ege Kutay <egekyurusen@gmail.com>
2022-05-13 00:31:23 +0300

    Lasttried....

--- index.js
+++ index.js
@@ -1,58 +1,53 @@
-const fs= require('fs')
+const fs = require("fs");
 //rows
-const swdayc=3
-let employees=[
-    {name:"İlknur KURBAN",swdaycount:swdayc,workingdays:[]},
-    {name:"Gamze AKYOL",swdaycount:swdayc,workingdays:[]},
-    {name:"Uğur CAN",swdaycount:swdayc,workingdays:[]},
-    {name:"Can UĞURLU",swdaycount:swdayc,workingdays:[]},
-    {name:"Emre OK",swdaycount:swdayc,workingdays:[]},
-    {name:"Volkan BALIKÇI",swdaycount:swdayc,workingdays:[]},
-    {name:"Nilden TUTALAR",swdaycount:swdayc,workingdays:[]},
-    {name:"Özgür SALGINCI",swdaycount:swdayc,workingdays:[]},
-    {name:"Mengüç HALİL",swdaycount:swdayc,workingdays:[]},
-    {name:"Gürkan",swdaycount:swdayc,workingdays:[]},
-]
+const swdayc = 3;
+let employees = [
+  { name: "İlknur KURBAN", swdaycount: swdayc, workingdays: [] },
+  { name: "Gamze AKYOL", swdaycount: swdayc, workingdays: [] },
+  { name: "Uğur CAN", swdaycount: swdayc, workingdays: [] },
+  { name: "Can UĞURLU", swdaycount: swdayc, workingdays: [] },
+  { name: "Emre OK", swdaycount: swdayc, workingdays: [] },
+  { name: "Volkan BALIKÇI", swdaycount: swdayc, workingdays: [] },
+  { name: "Nilden TUTALAR", swdaycount: swdayc, workingdays: [] },
+  { name: "Özgür SALGINCI", swdaycount: swdayc, workingdays: [] },
+  { name: "Mengüç HALİL", swdaycount: swdayc, workingdays: [] },
+  { name: "Gürkan", swdaycount: swdayc, workingdays: [] },
+];
 //max person allowed in office
-let maxpersonCount=Math.round(employees.length*0.5);
-employees=employees.sort(()=>Math.random() -0.5)
+let maxpersonCount = Math.round(employees.length * 0.5);
+employees = employees.sort(() => Math.random() - 0.5);
 //columns
-let days=[
-    {day:"Monday",employeeCount:6},
-    {day:"Tuesday",employeeCount:6},
-    {day:"Wednesday",employeeCount:6},
-    {day:"Thursday",employeeCount:6},
-    {day:"Friday",employeeCount:6},
-]
-let date=new Date()
+let days = [
+  { day: "Monday", employeeCount: 6 },
+  { day: "Tuesday", employeeCount: 6 },
+  { day: "Wednesday", employeeCount: 6 },
+  { day: "Thursday", employeeCount: 6 },
+  { day: "Friday", employeeCount: 6 },
+];
+let date = new Date();
+let daysIndex;
+for (let i = 0; i < employees.length; i++) {
+  let string1 = "";
+  let workingdays = JSON.parse(JSON.stringify(days));
+  for (let j = employees[i].swdaycount - 1; j >= 0; j--) {
+    //shuffling the days array here
 
-for(let i=0;i<employees.length;i++){
-    let string1=''
-    let workingdays=[...days]
-    while(true){
-       
-        if(employees[i].swdaycount>0){
-            workingdays=workingdays.sort(()=>Math.random() -0.5)
-            let daysIndex= days.findIndex(i=>i.day===workingdays[workingdays.length-1].day)
-            employees[i].workingdays.push(workingdays[workingdays.length-1].day)
-            workingdays.pop();
-            employees[i].swdaycount--;
-            days[daysIndex].employeeCount--
-            if(days[daysIndex].employeeCount<1){
-                days.splice(daysIndex,1)
-            }
-        }
-        else{
-            break;
-        }
-      
+    workingdays = workingdays.sort(() => Math.random() - 0.5);
 
+    let findIndex = days.findIndex((object) => {
+      return object.day === workingdays[workingdays.length - 1].day;
+    });
+    employees[i].workingdays.push(workingdays[workingdays.length - 1].day);
+    employees[i].swdaycount--;
+    console.log("----");
+    console.log(days[findIndex]);
+    console.log(workingdays[workingdays.length - 1]);
+    days[findIndex].employeeCount--;
+    workingdays.pop();
+    if (days[findIndex].employeeCount < 1) {
+      days.splice(daysIndex, 1);
     }
-     
-    console.log(employees[i])
-
+  }
 }
-console.log(days)
-
-
-
+console.log(employees);
+console.log(days);
